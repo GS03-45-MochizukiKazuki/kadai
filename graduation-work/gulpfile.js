f8ebcf8cdf9edf592106a1deef402f3885f46a66
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2016-02-25 10:42:32 +0900

    public / srcで分ける gulp追加

---
+++ graduation-work/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var sass = require('gulp-sass');
+
+// css
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+// image
+gulp.task("imagemin", function() {
+    gulp.watch("./src/img/*", function() {   
+        gulp.src("./src/img/*.{png,jpg,gif}")
+            .pipe(imagemin({
+            	progressive: true,
+    			use: [pngquant({quality: '65-80', speed: 1})]
+            }))
+            .pipe(gulp.dest('./public/img/'));
+    });
+});
+
+gulp.task('default', ['sass:watch', 'imagemin']);
